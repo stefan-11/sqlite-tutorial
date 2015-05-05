de65a7f9bf7fccfd91f25fe8019c5e0963773eca
Stefan Will <stefan-11@gmx.de>
2015-05-05 22:39:06 +0200

    new project

---
+++ SQLiteDBSample/SQLiteDBSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLiteDBSample
+//
+//  Created by Stefan Will on 05.05.15.
+//  Copyright (c) 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLiteDBSample/SQLiteDBSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQLiteDBSample
+//
+//  Created by Stefan Will on 05.05.15.
+//  Copyright (c) 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
